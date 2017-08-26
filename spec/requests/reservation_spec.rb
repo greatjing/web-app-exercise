8c814fd7aade4ca5e61ec8ed4fbc2d13ab83b341
greatjing <greatjing@sina.cn>
2017-08-26 22:24:18 +0800

    add check book seat info  and check delete book seat function

---
+++ spec/requests/reservation_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe "API_V1::Reservation", :type => :request do
+  before do
+    @train1 = Train.create!( :number => "0822" )
+    @train2 = Train.create!( :number => "0828" )
+
+    @user = User.create!( :email => "test@example.com", :password => "12345678" )
+    @reservation = Reservation.create!( :train_id => @train1.id, :seat_number => "1A",
+                                        :customer_name => "test", :customer_phone => "12345678" )
+  end
+
+  example "GET /api/v1/reservations/{booking_code}" do
+    get "/api/v1/reservations/#{@reservation.booking_code}"
+
+    expect(response).to have_http_status(200)
+    expect(response.body).to eq({ :booking_code => @reservation.booking_code,
+                                  :train_number => @reservation.train.number,
+                                  :train => {
+                                    :number => @train1.number,
+                                    :logo_url => nil,
+                                    :logo_file_size => nil,
+                                    :logo_content_type => nil,
+                                    :available_seats => ["1B", "1C", "2A", "2B", "2C", "3A", "3B", "3C", "4A", "4B", "4C", "5A", "5B", "5C", "6A", "6B", "6C" ],
+                                    :created_at => @train1.created_at
+                                  },
+                                  :seat_number => "1A",
+                                  :customer_name => "test",
+                                  :customer_phone => "12345678"
+                                }.to_json)
+  end
+
+  example "DELETE /api/v1/reservations/{booking_code}" do
+    delete "/api/v1/reservations/#{@reservation.booking_code}"
+
+    expect(response).to have_http_status(200)
+    expect(response.body).to eq({ :message => "已取消" }.to_json)
+    expect(Reservation.count).to eq(0)
+  end
+
+end
